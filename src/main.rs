cf8b703092860ae35edd18598ffc1f46706a71a2
Jasen Borisov <jamadazi@protonmail.ch>
2021-02-14 19:22:25 +0100

    Project Skeleton

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}; 
+
+fn main() {
+    App::build()
+        .insert_resource(ReportExecutionOrderAmbiguities)
+
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())   
+
+        .insert_resource(bevy::log::LogSettings {
+            level: bevy::log::Level::DEBUG,
+            ..Default::default()
+        })
+
+        .insert_resource(WindowDescriptor {
+            title: "SOTORA™ PRE-ALPHA".into(),
+            vsync: true,
+            resizable: true,
+            ..Default::default()
+        })
+
+        .insert_resource(ClearColor(Color::BLACK))
+
+        .add_plugins(DefaultPlugins)
+
+        .run();
+}
