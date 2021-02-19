584cb2745fce54790c505d42393f82a79c3814dd
Aaron Winter <winter.aaron93@gmail.com>
2021-02-19 09:00:19 +0100

    Use StageLabel (#27)
    * bump up bevy version, use enums for stages
    
    * cargo fmt

--- src/menu/mod.rs
+++ src/menu/mod.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 
 use crate::AppState;
+use crate::Stage;
 use crate::UiAssets;
-use crate::APPSTATES;
 
 pub mod main_menu;
 pub mod settings;
@@ -20,47 +20,55 @@ pub mod button {
 pub struct MenuPlugin;
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.on_state_enter(APPSTATES, AppState::MainMenu, main_menu::setup.system())
-            .on_state_update(
-                APPSTATES,
-                AppState::MainMenu,
-                button_interact::<button::ExitApp>
-                    .system()
-                    .chain(main_menu::button_exit_app.system()),
-            )
-            .on_state_update(
-                APPSTATES,
-                AppState::MainMenu,
-                button_interact::<button::EnterGame>
-                    .system()
-                    .chain(main_menu::button_enter_game.system()),
-            )
-            .on_state_update(
-                APPSTATES,
-                AppState::MainMenu,
-                button_interact::<button::OpenSettingsMenu>
-                    .system()
-                    .chain(main_menu::button_open_settings_menu.system()),
-            )
-            .on_state_exit(
-                APPSTATES,
-                AppState::MainMenu,
-                crate::despawn_all::<main_menu::StateCleanup>.system(),
-            )
-            // Settings menu
-            .on_state_enter(APPSTATES, AppState::SettingsMenu, settings::setup.system())
-            .on_state_update(
-                APPSTATES,
-                AppState::SettingsMenu,
-                button_interact::<button::ExitSettingsMenu>
-                    .system()
-                    .chain(settings::button_exit_settings_menu.system()),
-            )
-            .on_state_exit(
-                APPSTATES,
-                AppState::SettingsMenu,
-                crate::despawn_all::<settings::StateCleanup>.system(),
-            );
+        app.on_state_enter(
+            Stage::AppState,
+            AppState::MainMenu,
+            main_menu::setup.system(),
+        )
+        .on_state_update(
+            Stage::AppState,
+            AppState::MainMenu,
+            button_interact::<button::ExitApp>
+                .system()
+                .chain(main_menu::button_exit_app.system()),
+        )
+        .on_state_update(
+            Stage::AppState,
+            AppState::MainMenu,
+            button_interact::<button::EnterGame>
+                .system()
+                .chain(main_menu::button_enter_game.system()),
+        )
+        .on_state_update(
+            Stage::AppState,
+            AppState::MainMenu,
+            button_interact::<button::OpenSettingsMenu>
+                .system()
+                .chain(main_menu::button_open_settings_menu.system()),
+        )
+        .on_state_exit(
+            Stage::AppState,
+            AppState::MainMenu,
+            crate::despawn_all::<main_menu::StateCleanup>.system(),
+        )
+        // Settings menu
+        .on_state_enter(
+            Stage::AppState,
+            AppState::SettingsMenu,
+            settings::setup.system(),
+        )
+        .on_state_update(
+            Stage::AppState,
+            AppState::SettingsMenu,
+            button_interact::<button::ExitSettingsMenu>
+                .system()
+                .chain(settings::button_exit_settings_menu.system()),
+        )
+        .on_state_exit(
+            Stage::AppState,
+            AppState::SettingsMenu,
+            crate::despawn_all::<settings::StateCleanup>.system(),
+        );
     }
 }
 
